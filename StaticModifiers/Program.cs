37d6dfb693f1866a4a02cd24eecb97e1f77e8acd
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-05-18 20:59:48 +0600

    Add project files.

---
+++ StaticModifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using StaticModifiers.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticModifiers
+{
+
+    public class Person 
+    {
+        public string fName;
+        public string lName;
+
+        public void Introduce() 
+        { 
+            fName = Console.ReadLine();
+            lName = Console.ReadLine();
+            Console.WriteLine("Welcome , " + fName +" " + lName);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person shuvo = new Person();
+            shuvo.Introduce();
+
+            var result =  Calculator.Add(10,25);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
